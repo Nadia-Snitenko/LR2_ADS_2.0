b142ed06cf5e504efe1c95d515e8eddd601f99a1
N_20 <N_20@DESKTOP-GTN3FRP>
2022-04-12 19:31:48 +0400

    Добавлены функции для генерации отсортированных в прямом и обратном порядке векторов

--- Практики_АИСД_2.cpp
+++ Практики_АИСД_2.cpp
@@ -1,16 +1,26 @@
 ﻿#include <iostream>
 #include <vector>
 #include <time.h>
-#include <thread>         // this_thread::sleep_for – поток приостанавливает выполнение на некий, заданный промежуток времени
-#include <chrono>         // chrono::seconds
+#include<cmath>
+#include <thread>   // this_thread::sleep_for – поток приостанавливает выполнение на некий, заданный промежуток времени
+#include <chrono>   // chrono::seconds
 using namespace std;
 
 struct steady_clock;
+const double factor = 1.2473309; // фактор уменьшения
+const double _factor = 1/(1-exp(-1.62));
 
 struct stats {
 	size_t comparison_count = 0; // количество сравнений
 	size_t copy_count = 0; // количество копий
 };
+
+template<typename T, typename TCompare = std::less<T>>
+void Swap(T& i,T& j) {
+	T temp = i;
+	i = j;
+	j = temp;
+}
 
 template<typename T, typename TCompare = std::less<T>>
 stats selection_sort(vector<T>& data) {
@@ -27,7 +37,7 @@ stats selection_sort(vector<T>& data) {
 
 		stat.comparison_count++;
 		if (data[i] > data[min]) { 
-			stat.copy_count++; swap(data[i], data[min]); 
+			stat.copy_count++; Swap(data[i], data[min]); 
 		}
 	}
 	return stat;
@@ -36,7 +46,7 @@ stats selection_sort(vector<T>& data) {
 template<typename T, typename TCompare = std::less<T>>
 stats quick_sort_(vector<T>& vec, size_t left, size_t right) {
 	stats stat;
-	size_t i = left, j = right;
+	long long i = left, j = right;
 	T pivot = vec[left];
 	stat.copy_count++;
 	
@@ -95,7 +105,6 @@ template<typename T, typename TCompare = std::less<T>>
 stats comb_sort(vector<T>& data)
 {
 	stats stat;
-	double factor = 1.2473309; // фактор уменьшения
 	int step = data.size() - 1; // шаг сортировки
 
 	//Последняя итерация цикла, когда step==1 эквивалентна одному проходу сортировки пузырьком
@@ -106,7 +115,7 @@ stats comb_sort(vector<T>& data)
 			if (data[i] > data[i + step]) {
 
 				stat.copy_count++;
-				swap(data[i], data[i + step]);
+				Swap(data[i], data[i + step]);
 			}
 		}
 		step /= factor;
@@ -128,6 +137,20 @@ void fill(vector<T>& data, const size_t n) {
 };
 
 template<typename T, typename TCompare = std::less<T>>
+void fill_increasing(vector<T>& data, const size_t n) {
+	for (size_t i = 0; i < n; i++) {
+		data.push_back(i);
+	}
+};
+
+template<typename T, typename TCompare = std::less<T>>
+void fill_decreasing(vector<T>& data, const size_t n) {
+	for (size_t i = n; i >0 ; i--) {
+		data.push_back(i);
+	}
+};
+
+template<typename T, typename TCompare = std::less<T>>
 class V {
 private:
 	size_t size;
@@ -139,7 +162,9 @@ public:
 	}
 	V(size_t _size) {
 		size = _size;
-		fill(v, size);
+		//fill(v, size);
+		//fill_increasing(v, size);
+		fill_decreasing(v, size);
 	}
 	~V() {
 		v.clear();
@@ -182,7 +207,7 @@ int main() {
 	chrono::steady_clock::time_point begin = chrono::steady_clock::now();
 	chrono::steady_clock::time_point end = chrono::steady_clock::now();
 	srand(time(NULL));
-	const size_t size = 2000;
+	const size_t size = 4000;
 	
 	vector<V<int>> VV;
 	size_t c1, c2;
@@ -247,29 +272,32 @@ int main() {
 	stats s1, s2, s3;
 
 	fill(v1,size);
-	//cout << "vector 1 before sort: ";
-	//print(v1);
+	cout << "vector 1 before sort: ";
+	print(v1);
 	s1=selection_sort(v1);
-	//cout << "vector 1 after selection sort: ";
-	//print(v1);
+	cout << "vector 1 after selection sort: ";
+	print(v1);
 	cout << endl;
 
 	fill(v2, size);
-	/*cout << "vector 2 before sort: ";
-	print(v2);*/
+	cout << "vector 2 before sort: ";
+	print(v2);
 	s2=quick_sort(v2);
-	/*cout << "vector 2 after quick sort: ";
-	print(v2);*/
+	cout << "vector 2 after quick sort: ";
+	print(v2);
 	cout << endl;
 
 	fill(v3, size);
-	/*cout << "vector 3 before sort: ";
-	print(v3);*/
+	cout << "vector 3 before sort: ";
+	print(v3);
 	s3=comb_sort(v3);
-	/*cout << "vector 3 after comb sort: ";
-	print(v3);*/
+	cout << "vector 3 after comb sort: ";
+	print(v3);
 
-	cout << size << " elements:\n--------------\n";
+	
+
+
+	cout << size << " elements:\n";
 	cout << "sort   " << "     comparison count " << " copy count " << "\n";
 	cout << "__________________________________________\n";
 	cout <<"selection:       "<< s1.comparison_count << "          " << s1.copy_count << endl;
