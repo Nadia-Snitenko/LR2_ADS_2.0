037b82e6d2772ce933775551abfbd30fab4eccee
N_20 <N_20@DESKTOP-GTN3FRP>
2022-04-12 10:31:33 +0400

    добавлено измерение времени сортировок и класс для реализации подсчетов на 100 отсортированных векторов на одну сортировку

--- Практики_АИСД_2.cpp
+++ Практики_АИСД_2.cpp
@@ -1,61 +1,98 @@
 ﻿#include <iostream>
 #include <vector>
 #include <time.h>
-
+#include <thread>         // this_thread::sleep_for – поток приостанавливает выполнение на некий, заданный промежуток времени
+#include <chrono>         // chrono::seconds
 using namespace std;
+
+struct steady_clock;
 
 struct stats {
 	size_t comparison_count = 0; // количество сравнений
 	size_t copy_count = 0; // количество копий
 };
 
-stats selection_sort(vector<int>& data) {
+template<typename T, typename TCompare = std::less<T>>
+stats selection_sort(vector<T>& data) {
 	stats stat;
 	size_t min;
+
 	for (size_t i = 0; i < data.size() - 1; i++) {
 		min = i;
+
 		for (size_t j = i + 1; j < data.size(); j++) {
 			stat.comparison_count++;
 			if (data[j] < data[min]) min = j;
 		}
-		if (data[i] > data[min]) { swap(data[i], data[min]); stat.copy_count++; }
+
+		stat.comparison_count++;
+		if (data[i] > data[min]) { 
+			stat.copy_count++; swap(data[i], data[min]); 
+		}
 	}
 	return stat;
 };
 
-stats quick_sort_(vector<int>& vec, size_t left, size_t right) {
+template<typename T, typename TCompare = std::less<T>>
+stats quick_sort_(vector<T>& vec, size_t left, size_t right) {
 	stats stat;
 	size_t i = left, j = right;
-	int pivot = vec[left];
+	T pivot = vec[left];
+	stat.copy_count++;
+	
 	while (i < j) // пока границы не сомкнутся
 	{
-		while ((vec[j] >= pivot) && (i < j))
+		stat.comparison_count++;
+		while ((vec[j] >= pivot) && (i < j)) {
+			stat.comparison_count++; 
 			j--; // сдвигаем правую границу пока элемент [j] больше [pivot]
+		}
+		stat.comparison_count++; //+1 сравнение при выходе из цикла
+
+		stat.comparison_count++;
 		if (i != j) // если границы не сомкнулись
 		{
+			stat.copy_count++;
 			vec[i] = vec[j]; // перемещаем элемент [j] на место разрешающего
 			i++; // сдвигаем левую границу вправо
 		}
-		while ((vec[i] <= pivot) && (i < j))
+
+		while ((vec[i] <= pivot) && (i < j)) {
+			stat.comparison_count++; //+1
 			i++; // сдвигаем левую границу пока элемент [i] меньше [pivot]
+		}
+		stat.comparison_count++; //+1 сравнение при выходе из цикла
+
+		stat.comparison_count++;
 		if (i != j) // если границы не сомкнулись
 		{
+			stat.copy_count++;
 			vec[j] = vec[i]; // перемещаем элемент [i] на место [j]
 			j--; // сдвигаем правую границу влево
 		}
 	}
+	stat.comparison_count++;//+1 сравнение при выходе из цикла
+
+	stat.copy_count++;
 	vec[i] = pivot; // ставим разрешающий элемент на место
 	pivot = i;
+
+	stat.comparison_count++;
 	if (left < pivot) // Рекурсивно вызываем сортировку для левой и правой части массива
 		quick_sort_(vec, left, pivot - 1);
+
+	stat.comparison_count++; //+1 сравнение при выходе из цикла
 	if (right > pivot)
 		quick_sort_(vec, pivot + 1, right);
+
 	return stat;
 };
 
-stats quick_sort(vector<int>& data) { if (!data.empty()) return quick_sort_(data, 0, data.size() - 1); };
+template<typename T, typename TCompare = std::less<T>>
+stats quick_sort(vector<T>& data) { if (!data.empty()) return quick_sort_(data, 0, data.size() - 1); };
 
-stats comb_sort(vector<int>& data)
+template<typename T, typename TCompare = std::less<T>>
+stats comb_sort(vector<T>& data)
 {
 	stats stat;
 	double factor = 1.2473309; // фактор уменьшения
@@ -63,8 +100,12 @@ stats comb_sort(vector<int>& data)
 
 	//Последняя итерация цикла, когда step==1 эквивалентна одному проходу сортировки пузырьком
 	while (step >= 1) {
-		for (int i = 0; i + step < data.size(); i++) {
+		for (int i = 0; i + step < data.size(); i++){
+
+			stat.comparison_count++; 
 			if (data[i] > data[i + step]) {
+
+				stat.copy_count++;
 				swap(data[i], data[i + step]);
 			}
 		}
@@ -73,223 +114,167 @@ stats comb_sort(vector<int>& data)
 	return stat;
 };
 
-
-void print(vector<int>& data) {
+template<typename T, typename TCompare = std::less<T>>
+void print(vector<T>& data) {
 	for (auto i = data.begin(); i != data.end(); ++i) cout << *i << " ";
 	cout << endl;
 };
 
-void fill(vector<int>& data, const size_t n) {
+template<typename T, typename TCompare = std::less<T>>
+void fill(vector<T>& data, const size_t n) {
 	for (size_t i = 0; i < n; i++) {
-		data.push_back(rand() % 100);
+		data.push_back(rand()%100);
 	}
 };
 
-int main() {
-	vector<int> v1, v2,v3;
-	const size_t size = 10;
+template<typename T, typename TCompare = std::less<T>>
+class V {
+private:
+	size_t size;
+	vector<T> v;
+	stats s;
+public:
+	V() {
+		size = 0;
+	}
+	V(size_t _size) {
+		size = _size;
+		fill(v, size);
+	}
+	~V() {
+		v.clear();
+		size = 0;
+	}
 
+	void _selection_sort()
+	{
+		s=selection_sort(v);
+	}
+	void _quick_sort()
+	{
+		s = quick_sort(v);
+	}
+	void _comb_sort()
+	{
+		s = comb_sort(v);
+	}
+
+	void _print()
+	{
+		print(v);
+	}
+	void print_state()
+	{
+		cout<< s.comparison_count << "         " << s.copy_count << endl;
+	}
+	size_t _comparison_count()
+	{
+		return s.comparison_count;
+	}
+	size_t _copy_count()
+	{
+		return s.copy_count;
+	}
+};
+
+
+int main() {
+	chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+	chrono::steady_clock::time_point end = chrono::steady_clock::now();
 	srand(time(NULL));
+	const size_t size = 2000;
+	
+	vector<V<int>> VV;
+	size_t c1, c2;
+	c1 = c2 = 0;
+
+	begin = chrono::steady_clock::now();
+	for (size_t i = 0; i < 100; i++) {
+		VV.push_back(V<int>(size));
+		//VV[i]._print();
+		VV[i]._selection_sort();
+		c1 += VV[i]._comparison_count();
+		c2+= VV[i]._copy_count();
+	}
+	c1 /= 100;
+	c2 /= 100;
+
+	cout <<"for "<<size<<" elements:\n";
+	cout << "sort  "  <<"||" << "  comparison count " << "||" << "copy count " << "\n";
+	cout << "____________________________________________\n";
+	cout << "selection:"<<'\t' << c1 << '\t' << '\t' << c2 << endl;
+
+	end = std::chrono::steady_clock::now();
+	cout << "Diff(ms) = " << chrono::duration_cast<chrono::milliseconds>(end - begin).count() << endl;
+
+	VV.clear();
+	c1 = c2 = 0;
+
+	begin = chrono::steady_clock::now();
+	for (size_t i = 0; i < 100; i++) {
+		VV.push_back(V<int>(size));
+		//VV[i]._print();
+		VV[i]._quick_sort();
+		c1 += VV[i]._comparison_count();
+		c2 += VV[i]._copy_count();
+	}
+	c1 /= 100;
+	c2 /= 100;
+	cout << "quick:       " << '\t'<< c1 << '\t' << '\t' << c2 <<  endl;
+	end = std::chrono::steady_clock::now();
+	cout << "Diff(ms) = " << chrono::duration_cast<chrono::milliseconds>(end - begin).count() << endl;
+
+	VV.clear();
+	c1 = c2 = 0;
+
+	begin = chrono::steady_clock::now();
+	for (size_t i = 0; i < 100; i++) {
+		VV.push_back(V<int>(size));
+		//VV[i]._print();
+		VV[i]._comb_sort();
+		c1 += VV[i]._comparison_count();
+		c2 += VV[i]._copy_count();
+	}
+	c1 /= 100;
+	c2 /= 100;
+	cout << "comb:        " << '\t' << c1 << '\t' << '\t' << c2 << endl;
+
+	end = std::chrono::steady_clock::now();
+	cout << "Diff(ms) = " << chrono::duration_cast<chrono::milliseconds>(end - begin).count() << endl;
+
+//отдельные векторы с сортировками и c их states
+	vector<int> v1, v2, v3;
+	stats s1, s2, s3;
 
 	fill(v1,size);
-	cout << "vector 1 before sort: ";
-	print(v1);
-	selection_sort(v1);
-	cout << "vector 1 after selection sort: ";
-	print(v1);
+	//cout << "vector 1 before sort: ";
+	//print(v1);
+	s1=selection_sort(v1);
+	//cout << "vector 1 after selection sort: ";
+	//print(v1);
 	cout << endl;
 
 	fill(v2, size);
-	cout << "vector 2 before sort: ";
-	print(v2);
-	quick_sort(v2);
-	cout << "vector 2 after quick sort: ";
-	print(v2);
+	/*cout << "vector 2 before sort: ";
+	print(v2);*/
+	s2=quick_sort(v2);
+	/*cout << "vector 2 after quick sort: ";
+	print(v2);*/
 	cout << endl;
 
 	fill(v3, size);
-	cout << "vector 3 before sort: ";
-	print(v3);
-	int m = sizeof(v3) / sizeof(v3[0]);
-	comb_sort(v3);
-	cout << "vector 3 after comb sort: ";
-	print(v3);
+	/*cout << "vector 3 before sort: ";
+	print(v3);*/
+	s3=comb_sort(v3);
+	/*cout << "vector 3 after comb sort: ";
+	print(v3);*/
+
+	cout << size << " elements:\n--------------\n";
+	cout << "sort   " << "     comparison count " << " copy count " << "\n";
+	cout << "__________________________________________\n";
+	cout <<"selection:       "<< s1.comparison_count << "          " << s1.copy_count << endl;
+	cout <<"quick:           "<< s2.comparison_count << "          " << s2.copy_count << endl;
+	cout <<"comb:            "<< s3.comparison_count << "          " << s3.copy_count << endl;
 
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Практика 1
-//int main()
-//{
-//	size_t n= 5;
-//	vector<int>v(n);
-//	vector<int> v2;
-//	for (auto i : v)
-//	{
-//		v.push_back(rand() % 101+1);
-//
-//	}
-//	for (auto it = v.begin(); it != v.end(); it++) {
-//		cout << *it << ' ';
-//		if (!(*it % 2)) {
-//			v2.push_back(*it);
-//		}
-//	}
-//	cout << endl;
-//	for (auto it2 = v2.begin(); it2 != v2.end(); it2++) {
-//		cout << *it2 << ' ';
-//	}
-//	return 0;
-//}
-
-
-// Практика 2
-//
-//template<typename int>
-//class list {
-//	struct Node {
-//		T data;
-//		Node* next;
-//		Node* prev;
-//
-//	}*head;
-//	//, *tail;
-//	//size_t size;
-//public:
-//	list() { 
-//		//head = nullptr; 
-//		head.next = &head; 
-//		head.prev = &head;
-//		size = 0; }
-//	~list() {
-//		while (head.next != &head){
-//			Node* temp = head.next;
-//			head.next = head.next->next;
-//			delete temp;
-//		}
-//		head.prev = &head;
-//	}
-//	void Popfront() {
-//		if (head.next = &head) throw "empty";
-//		Node* temp = head.next;
-//		temp->next->prev = temp->prev;
-//		temp->prev->next = temp.next;
-//		delete temp;
-//	}
-//	void Pushfront(const T& new_data) {
-//		Node* new_elem = new Node;
-//		new_elem->data = new_data;
-//		new_elem->next = head.next;
-//		new_elem->prev = &head; 
-//		new_elem->next->prev = new_elem;
-//		new_elem->prev->next = new_elem;
-//	}
-//	friend class iterator;
-//	iterator begin() {
-//		return iterator(this, head->next);
-//	}
-//	iterator end() {
-//		return iterator(this, head);
-//	}
-//};
-//template <typename T> 
-//class iterator {
-//	list<T>* cont; // указатель на сам контейнер
-//	list<T>::node* current; // указатель на текущий элемент контейнера
-//public:
-//	iterator(list* new_cont, list <T> ::node* new_current) {
-//		current = new_corrent;
-//		cont = new_cont; 
-//	}
-//	iterator& operator++() {
-//		current = current->next;
-//		return*this;
-//	}
-//	bool operator ==(const iterator& rsh) {
-//		if (current == rsh.current) return true;
-//		else return false; 
-//	}
-//	bool operator!=(const iterator& rsh) {
-//		return(!current == rhs.current);
-//	}
-//	T operator*() {
-//		return current->data;
-//	}
-//};
-
-
-
-// Практика 3
-// 
-//int main()
-//{
-//    list <double> nums;
-//    nums.push_back(4.9);
-//    nums.push_back(7.09);
-//    nums.push_back(10.0);
-//    nums.push_back(2.644);
-//
-//    for ( int i : nums) {
-//        cout << i<<'\n';
-//    }
-//    return 0;
-//
-//}
-
-
-
-
-//Практика 4
-// 
-//template<typename T>
-//class stack {
-//    unsigned int size,max_size;
-//    T array;
-//public:
-//    stack(const unsigned int max_size) {
-//        size = 0;
-//        this->max_size = max_size;
-//        array = new T[max_size];
-//    }
-//    ~stack() {
-//        delete[] array;
-//    }
-//
-//    T& top() {
-//        if (size == 0) throw "empty";
-//        return array[size - 1];
-//    }
-//
-//    void push(const T new_el) {
-//        if (size == max_size) throw "max size";
-//        array[size] = new_el;
-//        size++; 
-//    }
-//    stack& operator=(const stack& s) {
-//        if (array == s.array) return *this;
-//        size = s.size;
-//        max_size = s.max_size;
-//        delete[] = array;
-//        array = new T[max_size];
-//        for (int i = 0; i < size; i++)
-//            array[i] = s.array[i];
-//        return *this;
-//    }
-//};
-//
-
